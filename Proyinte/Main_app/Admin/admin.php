8a3684bae6fdd27ef6e6257ec8d178a9961e17d1
JuanValencia <eteban.valencia@gmail.com>
2018-06-15 14:22:29 -0500

    Se crea inicio se sesion, usuario (Administrador - Usuario: Admin Constraseña: 123 -- Vendedor - Usuario: vendedor1 Contraseña: 1234) y formulario para ingreso de los registros.

---
+++ Proyinte/Main_app/Admin/admin.php
@@ -0,0 +1,40 @@
+<?php
+ ?>
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>Admin</title>
+         <link rel="stylesheet" href="../../css/main.css">
+         <script src="../../js/jquery-3.3.1.min.js"></script>
+          <script src="../../js/main.js"></script>
+   </head>
+   <body>
+<h1>Bienvenido Administrador</h1>
+        <div class="main">
+        <form action="" id="formLg">
+        <input type="text" name="cliente"  placeholder="Cliente" required>
+        <input type="text" name="nit"  placeholder="Nit" required>
+        <label for="tipoProd">Seleccion Producto</label>
+         <select class="form-control" id="tipoProd" placeholder="Nit">
+           <option>Hidromasaje</option>
+           <option>Lavamanos</option>
+           <option>Lavarropas</option>
+           <option>Cocinas</option>
+           <option>Lavaplatos</option>
+         </select>
+         <input type="text" name="cantidad"  placeholder="Cantidad" required>
+         <label for="tipoProd">Tipo de Novedad</label>
+         <select class="form-control" id="tipoProd" placeholder="Nit">
+           <option>Peticion</option>
+           <option>Queja</option>
+           <option>Reclamo</option>
+           <option>Sugerencia</option>
+          </select>
+        <textarea  name="Mensaje" maxlength="1000" cols="25" rows="6" placeholder="Descripcion"></textarea>
+        
+        <input type="submit" class="botonlg"  value="Reportar" >
+     </form>
+     
+   </body>
+ </html>

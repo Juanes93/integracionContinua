dceaa7345e2e08fe75bc7fdc8dff28700e4e1ed5
PedroLuisPereira <36006865+PedroLuisPereira@users.noreply.github.com>
2018-06-17 22:57:52 -0500

    NuevoUsuario
    Se crea un nuevo formula para crear nuevos usuarios de la aplicacion

---
+++ Proyinte/Main_app/Usuario/NuevoUsuario.php
@@ -0,0 +1,26 @@
+<?php
+ ?>
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>Admin</title>
+         <link rel="stylesheet" href="../../css/main.css">
+         <script src="../../js/jquery-3.3.1.min.js"></script>
+          <script src="../../js/main.js"></script>
+   </head>
+   <body>
+<h1>Nuevo Usuario</h1>
+        <div class="main">
+        <form action="" id="formLg">
+        
+            <input type="text" name="nombre"  placeholder="Nombre" required>
+            <input type="text" name="cedula"  placeholder="Cedula" required>
+            <input type="text" name="mail"  placeholder="Mail" required>
+            <input type="password" name="pass"  placeholder="Contraseña" required>
+            <input type="password" name="pass"  placeholder="Confirmar Contraseña" required>
+        
+     </form>
+     
+   </body>
+ </html>

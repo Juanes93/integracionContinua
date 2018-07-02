26bbdc3773978e1b68a640c9b4615bd211b6a150
fremol <fremol2002@yahoo.com>
2018-07-02 12:35:34 -0500

    Actualización index
    Modificación de aspectos gráficos y de la distribución de la información

--- development/code/proyinte/index.php
+++ development/code/proyinte/index.php
@@ -4,20 +4,35 @@
     <meta charset="utf-8">
     <title>Proyinte</title>
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css"> <!--Hoja de estilos con los íconos-->
   </head>
   <body>
-    <div class="error">
-      <span>Datos de ingreso no válidos, inténtelo de nuevo  por favor</span>
-    </div>
-    <div class="main">
-     <form action="" id="formLg">
-        <input type="text" name="usuariolg"  placeholder="Usuario" required>
-        <input type="password" name="passlg"  placeholder="Contraseña" required>
-        <input type="submit" class="botonlg"  value="Iniciar Sesion" >
-     </form>
+    <header class="site-header">
+          <div class="logo">
+              <img src="img/logo2.jpg" alt="logo pqrs">
+          </div>
+          <div class="navegacion">
 
-    </div>
+          </div>
+          <div class="hero">
+            <div class="contenido-header">
+              <div class="autentica">
+                <div class="error">
+                  <span>Datos de ingreso no válidos, inténtelo de nuevo  por favor</span>
+                </div>
+                <div class="main">
+                  <form action="" id="formLg">
+                    <input type="text" name="usuariolg"  placeholder="Usuario" required>
+                    <input type="password" name="passlg"  placeholder="Contraseña" required>
+                    <input type="submit" class="botonlg"  value="Iniciar Sesion" >
+                  </form>
+                </div>
+              </div> <!--autentica-->
+            </div> <!--contenido-header-->
+          </div> <!--hero-->
+
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/main.js"></script>
+    </header>
   </body>
 </html>
